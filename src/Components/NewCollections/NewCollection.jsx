edb2e1dfabe61a56a8946dda4ae41c4620b70e7c
pranali-kurumkar <pranalikurumkar07@gmail.com>
2025-04-14 23:30:12 +0530

    Update NewCollection.jsx

--- src/Components/NewCollections/NewCollection.jsx
+++ src/Components/NewCollections/NewCollection.jsx
@@ -7,7 +7,7 @@ const NewCollection = () => {
   const [new_collections ,setNew_collections] = useState([])
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://e-commerce-backend-53cq.onrender.com/newcollections')
     .then((response) =>response.json())
     .then((data)=> setNew_collections(data))
 
